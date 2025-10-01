ba3875e5795fdbab6f16a72da86fdc3ec42d5cf3
Joseph Trilli <rebooteed@gmail.com>
2025-10-01 11:15:25 -0400

    fixed: ranking

--- RaceWinners/RaceWinners/Program.cs
+++ RaceWinners/RaceWinners/Program.cs
@@ -23,7 +23,7 @@ public class Program
         }
         // take all of the students in each class up to the length of D class, which has 17 students
         // therefore we take the first 17 students from each class
-        // to determine the winner we'll take the class with the highest average rank
+        // to determine the winner we'll take the class with the lowest average rank
         var winner = data
             .Select(g => new Student 
             { 
@@ -31,20 +31,21 @@ public class Program
                 Ranks = g.Ranks.Take(17).ToList() 
             })
             .OrderBy(s => s.Ranks.Average())
-            .Last();
+            .First();
         // now that the winner is determined, print out the winning class and average rank of the other classes in descending order
         Console.WriteLine($"\nThe winning class is {winner.Name} with an average rank of {winner.Ranks.Average():F2}.\n");
         Console.WriteLine("\nThis was determined by getting the average rank of the first 17 students. All 4 classes' ranks were determined this way.");
         Console.WriteLine("\nOther classes in order:");
-        // get the other classes in order of average rank descending
+        // get the other classes in order of average rank ascending
+        // we can reuse the same logic as above, but filter out the winner
         var otherClasses = data
-            .Where(g => g.Name != winner.Name)
             .Select(g => new Student 
             { 
                 Name = g.Name, 
                 Ranks = g.Ranks.Take(17).ToList() 
             })
-            .OrderByDescending(s => s.Ranks.Average());
+            .Where(s => s.Name != winner.Name)
+            .OrderBy(s => s.Ranks.Average());
 
         foreach (var s in otherClasses) 
         {             
